fb16d5def2412ef03a2219955307a4f7e3b24b19
Luca Deltodesco <luca@deltaluca.me.uk>
2014-06-01 18:10:30 +0100

    Clean up source.

--- ttfcompile/distanceTransform.cpp
+++ ttfcompile/distanceTransform.cpp
@@ -5,190 +5,183 @@
 #include <functional>
 #include <set>
 #include <cmath>
+#include <cassert>
 
-using namespace std;
+using std::size_t;
 
 void distanceTransform(const png::image<png::gray_pixel>& image,
                        png::image<png::gray_pixel>& dist,
-                       pair<int,int> insize,
-                       pair<int,int> outsize,
-                       int searchsize) {
-    float scaleX = ((float)insize.first) /outsize.first;
-    float scaleY = ((float)insize.second)/outsize.second;
+                       std::pair<size_t, size_t> insize,
+                       std::pair<size_t, size_t> outsize,
+                       size_t searchsize)
+{
+    float scaleX = ((float)insize.first)  / outsize.first;
+    float scaleY = ((float)insize.second) / outsize.second;
 
     // Raw distance map.
-    float* hood = new float[outsize.first*outsize.second];
+    float* hood = new float[outsize.first * outsize.second];
     float min = 1e20f;
     float max = -1e20f;
 
-    auto lerp = [&](float x, float y) {
+    auto lerp = [&](float x, float y)
+    {
         if (x < 0 || y < 0 || x >= insize.first || y >= insize.second)
+        {
             return 1e20f;
+        }
 
         int x0 = (int)x;
         int y0 = (int)y;
-        int x1 = x0+1; if (x1 >= insize.first) x1 = insize.first-1;
-        int y1 = y0+1; if (y1 >= insize.second) y1 = insize.second-1;
+        int x1 = x0 + 1;
+        int y1 = y0 + 1;
+        if (y1 >= insize.second)
+        {
+            y1 = insize.second - 1;
+        }
+        if (x1 >= insize.first)
+        {
+            x1 = insize.first  - 1;
+        }
 
-        float fx = x-x0;
-        float fy = y-y0;
+        float fx = x - x0;
+        float fy = y - y0;
 
         int g00 = image[y0][x0];
         int g10 = image[y0][x1];
         int g11 = image[y1][x1];
         int g01 = image[y1][x0];
 
-        return ((1-fx)*(1-fy)*g00 + (1-fx)*fy*g01 + fx*(1-fy)*g10 + fx*fy*g11)-0x80;
+        return ((1 - fx) * (1 - fy) * g00 +
+                (1 - fx) * fy * g01 +
+                fx * (1 - fy) * g10 +
+                fx * fy * g11) - 0x80;
     };
 
-    vector<pair<float,vector<pair<float,float>>>> deltas;
-    for (int i = 1; i <= searchsize*2; i++) {
-        float r = ((float)i)/2;
-        int N = 2*3.14159265358979323846*r;
-        vector<pair<float,float>> ring;
-        for (int j = 0; j < N; j++) {
-            ring.push_back(pair<float,float>(
-                r*cos(j*3.14159265358979323846*2/N), r*sin(j*3.14159265358979323846*2/N)
+    std::vector<std::pair<float, std::vector<std::pair<float, float>>>> deltas;
+    for (size_t i = 1; i <= searchsize * 2; ++i)
+    {
+        float r = ((float)i) / 2;
+        size_t N = 2 * 3.14159265358979323846 * r;
+        std::vector<std::pair<float,float>> ring;
+        for (size_t j = 0; j < N; ++j)
+        {
+            ring.push_back(std::pair<float, float>(
+                r * cos(j * 3.14159265358979323846 * 2 / N),
+                r * sin(j * 3.14159265358979323846 * 2 / N)
             ));
         }
-        deltas.push_back(pair<float,vector<pair<float,float>>>(r, ring));
+        deltas.push_back(std::pair<float, std::vector<std::pair<float, float>>>(r, ring));
     }
 
+    for (size_t iy = 0; iy < outsize.second; ++iy)
+    {
+        for (size_t ix = 0; ix < outsize.first; ++ix)
+        {
+            // Pixel location on src image.
+            float bx = (ix+0.5)*scaleX;
+            float by = (iy+0.5)*scaleY;
 
-    for (int iy = 0; iy < outsize.second; iy++) {
-    for (int ix = 0; ix < outsize.first; ix++) {
-        // Pixel location on src image.
-        float bx = (ix+0.5)*scaleX;
-        float by = (iy+0.5)*scaleY;
+            float m = lerp(bx, by);
+            bool inside = m >= 0;
+            float minDistance = 1e20f;
+            if (m != 0)
+            {
+                // Radial outwards search.
+                for (auto ring: deltas)
+                {
+                    for (auto delta: ring.second)
+                    {
+                        std::function<float(int, float, float, float, float)> binary;
+                        binary = [&](int N, float v0, float v1, float k0, float k1)
+                        {
+                            float k = k0 + (k1 - k0) * (0 - v0) / (v1 - v0);
+                            float vk = lerp(bx + delta.first * k, by + delta.second * k);
 
-        float m = lerp(bx, by);
-        bool inside = m >= 0;
-        float minDistance = 1e20f;
-        if (m != 0) {
-            // Radial outwards search.
-            for (auto ring: deltas) {
-                for (auto delta: ring.second) {
-                    function<float(int,float,float,float,float)> binary = [&](int N, float v0, float v1, float k0, float k1) {
-                        float k = k0 + (k1-k0)*(0 - v0)/(v1 - v0);
-                        float vk = lerp(bx + delta.first*k, by + delta.second*k);
-                        if (vk*vk < 1e-5 || N > 20) return k;
-                        if (v0*vk < 0) return binary(N+1,v0, vk, k0, k);
-                        if (vk*v1 < 0) return binary(N+1,vk, v1, k, k1);
-                    };
-                    float n = lerp(bx + delta.first, by + delta.second);
-                    if (n != 1e20f && n*m <= 0) {
-                        float dist = ring.first;
-                        if (n == 0) minDistance = dist;
-                        else minDistance = dist * binary(0,m, n, 0, 1);
+                            if (vk*vk < 1e-5 || N > 20)
+                            {
+                                return k;
+                            }
+                            if (v0*vk < 0)
+                            {
+                                return binary(N + 1, v0, vk, k0, k);
+                            }
+                            if (vk*v1 < 0)
+                            {
+                                return binary(N+1,vk, v1, k, k1);
+                            }
+
+                            assert(false);
+                            return k;
+                        };
+
+                        float n = lerp(bx + delta.first, by + delta.second);
+                        if (n != 1e20f && n*m <= 0)
+                        {
+                            float dist = ring.first;
+                            if (n == 0)
+                            {
+                                minDistance = dist;
+                            }
+                            else
+                            {
+                                minDistance = dist * binary(0, m, n, 0, 1);
+                            }
+                        }
+                    }
+                    if (minDistance != 1e20f)
+                    {
+                        break;
                     }
                 }
-                if (minDistance != 1e20f) break;
             }
-        }
-        else minDistance = 0;
+            else
+            {
+                minDistance = 0;
+            }
 
-        if (minDistance != 1e20f) {
-            if (!inside) minDistance = -minDistance;
-            if (minDistance < min) min = minDistance;
-            if (minDistance > max) max = minDistance;
+            if (minDistance != 1e20f)
+            {
+                if (!inside)
+                {
+                    minDistance = -minDistance;
+                }
+                min = std::min(minDistance, min);
+                max = std::max(minDistance, max);
+            }
+            else if (!inside)
+            {
+                minDistance = -minDistance;
+            }
+            hood[iy * outsize.first + ix] = minDistance;
         }
-        else if (!inside) minDistance = -minDistance;
-        hood[iy*outsize.first+ix] = minDistance;
-    }}
+    }
 
     // Perform scaling.
     float mm = fmax(fabs(min), fabs(max));
     min = -mm;
     max = mm;
     float det = 255.0/(max-min);
-    for (int iy = 0; iy < outsize.second; iy++) {
-    for (int ix = 0; ix < outsize.first; ix++) {
-        float m = hood[iy*outsize.first+ix];
-        if (m == 1e20f) m = 255;
-        else if (m == -1e20f) m = 0;
-        else
-            m = (m - min)*det;
-        dist[iy][ix] = (int)m;
-    }}
-
-    delete [] hood;
-}
-
-void distanceTraxnsform(const png::image<png::gray_pixel>& image,
-                       png::image<png::gray_pixel>& dist,
-                       pair<int,int> insize,
-                       pair<int,int> outsize,
-                       int searchsize) {
-   float scaleX = ((float)insize.first) /outsize.first;
-   float scaleY = ((float)insize.second)/outsize.second;
-
-   auto lerp = [&](float x, float y) {
-        if (x < 0 || y < 0 || x >= insize.first || y >= insize.second)
-            return 1e20f;
-
-        int x0 = (int)x;
-        int y0 = (int)y;
-        int x1 = x0+1; if (x1 >= insize.first) x1 = insize.first-1;
-        int y1 = y0+1; if (y1 >= insize.second) y1 = insize.second-1;
-
-        float fx = x-x0;
-        float fy = y-y0;
-
-        int g00 = image[y0][x0];
-        int g10 = image[y0][x1];
-        int g11 = image[y1][x1];
-        int g01 = image[y1][x0];
-
-        return ((1-fx)*(1-fy)*g00 + (1-fx)*fy*g01 + fx*(1-fy)*g10 + fx*fy*g11)-0x80;
-    };
-
-    float* hood = new float[outsize.first*outsize.second];
-    float min = 1e20f;
-    float max = -1e20f;
-
-    for (int iy = 0; iy < outsize.second; iy++) {
-    for (int ix = 0; ix < outsize.first; ix++) {
-        float m = lerp((ix+0.5)*scaleX, (iy+0.5)*scaleY);
-        bool inside = m >= 0;
-
-        float minDistance = 1e20f;
-        for (int y = -searchsize; y <= searchsize; y++) {
-            float ay = ((iy+0.5)*scaleY) + y;
-            for (int x = -searchsize; x <= searchsize; x++) {
-                float ax = ((ix+0.5)*scaleX) + x;
-
-                float n = lerp(ax, ay);
-                if (n == 1e20f) continue; // outside
-                if (n*m <= 0) {
-                    float dist = sqrt(x*x+y*y);
-                    if (dist < minDistance) minDistance = dist;
-                }
+    for (size_t iy = 0; iy < outsize.second; ++iy)
+    {
+        for (size_t ix = 0; ix < outsize.first; ++ix)
+        {
+            float m = hood[iy * outsize.first + ix];
+            if (m == 1e20f)
+            {
+                m = 255;
             }
+            else if (m == -1e20f)
+            {
+                m = 0;
+            }
+            else
+            {
+                m = (m - min) * det;
+            }
+            dist[iy][ix] = (int)m;
         }
-        if (minDistance != 1e20f) {
-            if (!inside) minDistance = -minDistance;
-            if (minDistance < min) min = minDistance;
-            if (minDistance > max) max = minDistance;
-        }
-        else {
-            if (!inside) minDistance = -minDistance;
-        }
-        hood[iy*outsize.first+ix] = minDistance;
-    }}
-
-    float mm = fmax(fabs(min), fabs(max));
-    min = -mm;
-    max = mm;
-    float det = 255.0/(max-min);
-    for (int iy = 0; iy < outsize.second; iy++) {
-    for (int ix = 0; ix < outsize.first; ix++) {
-        float m = hood[iy*outsize.first+ix];
-        if (m == 1e20f) m = 255;
-        else if (m == -1e20f) m = 0;
-        else
-            m = (m - min)*det;
-        dist[iy][ix] = (int)m;
-    }}
+    }
 
     delete [] hood;
 }
+
--- ttfcompile/packing.cpp
+++ ttfcompile/packing.cpp
@@ -1,21 +1,23 @@
 #include <map>
 #include <vector>
 #include <set>
+#include <cstdint>
 
-using namespace std;
+using std::size_t;
 
 // Best fit decreasing.
 // Thanks to jakesgordon
 // https://github.com/jakesgordon/bin-packing
 struct Node {
-    int x, y, w, h;
+    size_t x, y, w, h;
     bool used;
     Node* down;
     Node* right;
 };
 Node* root = nullptr;
 
-Node* node(int x, int y, int w, int h) {
+Node* node(size_t x, size_t y, size_t w, size_t h)
+{
     auto ret = new Node();
     ret->x = x;
     ret->y = y;
@@ -26,34 +28,51 @@ Node* node(int x, int y, int w, int h) {
     return ret;
 }
 
-void destroyNode(Node* n) {
-    if (n == nullptr) return;
+void destroyNode(Node* n)
+{
+    if (n == nullptr)
+    {
+        return;
+    }
+
     destroyNode(n->down);
     destroyNode(n->right);
     delete n;
 }
 
-Node* findNode(Node* root, int w, int h) {
-    if (root->used) {
+Node* findNode(Node* root, size_t w, size_t h)
+{
+    if (root->used)
+    {
         auto a = findNode(root->down, w, h);
-        if (a == nullptr) a = findNode(root->right, w, h);
+        if (a == nullptr)
+        {
+            a = findNode(root->right, w, h);
+        }
         return a;
     }
     else if (w <= root->w && h <= root->h)
-         return root;
-    else return nullptr;
+    {
+        return root;
+    }
+    else
+    {
+        return nullptr;
+    }
 }
 
-Node* splitNode(Node* root, int w, int h) {
+Node* splitNode(Node* root, size_t w, size_t h)
+{
     root->used = true;
     root->down  = node(root->x,   root->y+h, root->w,   root->h-h);
     root->right = node(root->x+w, root->y,   root->w-w, h);
     return root;
 }
 
-Node* growRight(int w, int h);
-Node* growDown(int w, int h);
-Node* growNode(int w, int h) {
+Node* growRight(size_t w, size_t h);
+Node* growDown(size_t w, size_t h);
+Node* growNode(size_t w, size_t h)
+{
     bool canGrowDown  = w <= root->w;
     bool canGrowRight = h <= root->h;
 
@@ -61,14 +80,30 @@ Node* growNode(int w, int h) {
     bool shouldGrowRight = canGrowRight && (root->h >= (root->w + w));
     bool shouldGrowDown  = canGrowDown  && (root->w >= (root->h + h));
 
-    if      (shouldGrowRight) return growRight(w,h);
-    else if (shouldGrowDown)  return growDown (w,h);
-    else if (canGrowRight)    return growRight(w,h);
-    else if (canGrowDown)     return growDown (w,h);
-    else return nullptr;
+    if (shouldGrowRight)
+    {
+        return growRight(w,h);
+    }
+    else if (shouldGrowDown)
+    {
+        return growDown (w,h);
+    }
+    else if (canGrowRight)
+    {
+        return growRight(w,h);
+    }
+    else if (canGrowDown)
+    {
+        return growDown (w,h);
+    }
+    else
+    {
+        return nullptr;
+    }
 }
 
-Node* growRight(int w, int h) {
+Node* growRight(size_t w, size_t h)
+{
     auto newRoot = node(0, 0, root->w+w, root->h);
     newRoot->used = true;
     newRoot->down = root;
@@ -76,11 +111,17 @@ Node* growRight(int w, int h) {
     root = newRoot;
     auto node = findNode(root, w, h);
     if (node != nullptr)
+    {
          return splitNode(node, w, h);
-    else return nullptr;
+    }
+    else
+    {
+        return nullptr;
+    }
 }
 
-Node* growDown(int w, int h) {
+Node* growDown(size_t w, size_t h)
+{
     auto newRoot = node(0, 0, root->w, root->h+h);
     newRoot->used = true;
     newRoot->down = node(0, root->h, root->w, h);
@@ -88,29 +129,42 @@ Node* growDown(int w, int h) {
     root = newRoot;
     auto node = findNode(root, w, h);
     if (node != nullptr)
+    {
          return splitNode(node, w, h);
-    else return nullptr;
+    }
+    else
+    {
+        return nullptr;
+    }
 }
 
-void pack_boxes(const vector<pair<int,int>>& sizes, int gap,
-                map<int,pair<int,int>>* _packing,
-                pair<int,int>* _size) {
-    map<int,pair<int,int>>& packing = *_packing;
-    pair<int,int>& size = *_size;
-    int N = sizes.size();
-    if (N == 0) return;
+void pack_boxes(const std::vector<std::pair<size_t, size_t>>& sizes, size_t gap,
+                std::map<size_t, std::pair<size_t, size_t>>& packing,
+                std::pair<size_t, size_t>& size)
+{
+    size_t N = sizes.size();
+    if (N == 0)
+    {
+        return;
+    }
 
     // Sort element ids by box size. (Simple brute force.)
-    set<int> processed;
-    vector<int> backRef;
-    for (int i = 0; i < N; i++) {
-        int max = -1;
-        int maxSize = 0;
-        for (int j = 0; j < N; j++) {
-            if (processed.find(j) != processed.end()) continue;
-            const pair<int,int>& box = sizes[j];
-            int size = box.first > box.second ? box.first : box.second;
-            if (size > maxSize || max == -1) {
+    std::set<size_t> processed;
+    std::vector<size_t> backRef;
+    for (size_t i = 0; i < N; ++i)
+    {
+        size_t max = -1;
+        size_t maxSize = 0;
+        for (size_t j = 0; j < N; j++)
+        {
+            if (processed.find(j) != processed.end())
+            {
+                continue;
+            }
+            auto& box = sizes[j];
+            size_t size = box.first > box.second ? box.first : box.second;
+            if (size > maxSize || max == -1)
+            {
                 max = j;
                 maxSize = size;
             }
@@ -118,18 +172,26 @@ void pack_boxes(const vector<pair<int,int>>& sizes, int gap,
         backRef.push_back(max);
         processed.insert(max);
     }
-    const auto& getBlock = [&](int n) {
+    auto getBlock = [&](size_t n)
+    {
         return sizes[backRef[n]];
     };
 
-    root = node(0,0,getBlock(0).first+gap*2,getBlock(0).second+gap*2);
-    for (int i = 0; i < N; i++) {
+    root = node(0, 0, getBlock(0).first + gap * 2, getBlock(0).second + gap * 2);
+    for (size_t i = 0; i < N; ++i)
+    {
         const auto& block = getBlock(i);
-        Node* node = findNode(root, block.first+gap*2, block.second+gap*2);
+        Node* node = findNode(root, block.first + gap * 2, block.second + gap * 2);
         Node* fit;
-        if (node != nullptr) fit = splitNode(node, block.first+gap*2, block.second+gap*2);
-        else fit = growNode(block.first+gap*2, block.second+gap*2);
-        packing[backRef[i]] = pair<int,int>(fit->x + gap, fit->y + gap);
+        if (node != nullptr)
+        {
+            fit = splitNode(node, block.first + gap * 2, block.second + gap * 2);
+        }
+        else
+        {
+            fit = growNode(block.first + gap * 2, block.second + gap * 2);
+        }
+        packing[backRef[i]] = std::pair<size_t,size_t>(fit->x + gap, fit->y + gap);
     }
 
     size.first = root->w;
